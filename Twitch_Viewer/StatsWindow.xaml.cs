e2d112f4aa1a6b93d7e7b399e633143b4e844a24
Masamas <neuanmeldungen@yahoo.de>
2016-08-31 16:44:14 +0200

    update streamStats graph when new stats are added added background to graph value for better readabilty

--- Twitch_Viewer/StatsWindow.xaml.cs
+++ Twitch_Viewer/StatsWindow.xaml.cs
@@ -30,6 +30,8 @@ namespace Twitch_Viewer
             //for (int i = 0; i < 50; i++)
             //    stats.ViewTimeData.Add(new ViewTimeData(new DateTime(2016, 8, rnd.Next(1, 32), rnd.Next(24), rnd.Next(60), rnd.Next(60)), new TimeSpan(rnd.Next(1), rnd.Next(1, 60), rnd.Next(60))));
 
+            stats.ViewTimeData.Sort(item => item.Start);
+
             InitializeComponent();
 
             this.DataContext = stats;
