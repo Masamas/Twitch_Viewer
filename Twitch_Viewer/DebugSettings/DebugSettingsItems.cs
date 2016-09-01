0bfd56503909bd2f6664eb4546752d3a73168e86
Masamas <neuanmeldungen@yahoo.de>
2016-09-01 13:37:20 +0200

    add INotifyPropertyChanged interface to DebugSettingsItem, add DebugSettingButtonWithValue, refactoring of DebugSettings status

--- Twitch_Viewer/DebugSettings/DebugSettingsItems.cs
+++ Twitch_Viewer/DebugSettings/DebugSettingsItems.cs
@@ -1,23 +1,28 @@
 ﻿using System;
+using System.ComponentModel;
 using System.Linq;
+using System.Reflection;
 using System.Windows;
 using System.Windows.Input;
 
 namespace Twitch_Viewer
 {
-    public abstract class DebugSettingsItem
+    public abstract class DebugSettingsItem : INotifyPropertyChanged
     {
         public string Description { get; set; }
 
         public Visibility IsCheckBoxVisible { get; set; } = Visibility.Collapsed;
         public Visibility IsTextBoxVisible { get; set; } = Visibility.Collapsed;
+        public Visibility IsValueBoxVisible { get; set; } = Visibility.Collapsed;
         public Visibility IsButtonVisible { get; set; } = Visibility.Collapsed;
         public Visibility IsSeparatorVisible { get; set; } = Visibility.Collapsed;
+
+        public event PropertyChangedEventHandler PropertyChanged;
 
         protected DebugSettingsItem(string settingName, string description)
         {
             Description = description;
-            if (this is DebugSettingCheckBox || this is DebugSettingTextBox)
+            if (this is DebugSettingCheckBox || this is DebugSettingTextBox || this is DebugSettingButtonWithValue)
                 MainWindow.settings.DebugSettings.Add(settingName, this);
         }
 
@@ -28,6 +33,17 @@ namespace Twitch_Viewer
 
         protected DebugSettingsItem()
         {
+        }
+
+        protected void OnPropertyChanged(MethodBase methodBase)
+        {
+            OnPropertyChanged(methodBase.Name.Substring(4));
+        }
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 
@@ -45,7 +61,8 @@ namespace Twitch_Viewer
 
     public class DebugSettingTextBox : DebugSettingsItem
     {
-        public string TextBoxText { get; set; } = "";
+        private string _textBoxText = "";
+        public string TextBoxText { get { return _textBoxText; } set { _textBoxText = value; OnPropertyChanged(MethodBase.GetCurrentMethod()); } }
 
         public DebugSettingTextBox(string settingName, string description)
             : base(settingName, description)
@@ -86,6 +103,29 @@ namespace Twitch_Viewer
 
             IsButtonVisible = Visibility.Visible;
             ButtonText = buttonText;
+        }
+
+        protected DebugSettingButton(string settingName, string description, string buttonText, buttonActionDelegate action, params object[] actionParameters)
+            : base(settingName, description)
+        {
+            _buttonAction = action;
+            _buttonActionParameters = actionParameters;
+
+            IsButtonVisible = Visibility.Visible;
+            ButtonText = buttonText;
+        }
+    }
+
+    public class DebugSettingButtonWithValue : DebugSettingButton
+    {
+        private string _value = "";
+        public string Value { get { return _value; } set { _value = value; OnPropertyChanged(MethodBase.GetCurrentMethod()); } }
+
+        public DebugSettingButtonWithValue(string settingName, string description, string buttonText, buttonActionDelegate action, string startValue = "", params object[] actionParameters)
+            : base(settingName, description, buttonText, action, actionParameters)
+        {
+            _value = startValue;
+            IsValueBoxVisible = Visibility.Visible;
         }
     }
 
--- Twitch_Viewer/DebugSettings/DebugSettingsWindow.xaml.cs
+++ Twitch_Viewer/DebugSettings/DebugSettingsWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Windows;
 
@@ -11,40 +13,49 @@ namespace Twitch_Viewer
     public partial class DebugSettingsWindow : Window
     {
         public ObservableCollection<DebugSettingsItem> DebugSettingsItems { get; } = new ObservableCollection<DebugSettingsItem>();
+        public Dictionary<string, DebugSettingsItem> DebugSettings { get { return MainWindow.settings.DebugSettings; } }
 
         public DebugSettingsWindow()
         {
             InitializeComponent();
 
-            DebugSettingsItems.Add(new DebugSettingCheckBox("statsLimit", "Disable 30 second limit on stats ?", true));
-            DebugSettingsItems.Add(new DebugSettingTextBox("randomStatsUsername", "User to add Random Stats to:"));
-            DebugSettingsItems.Add(new DebugSettingTextBox("randomStatsCount", "Number of Random Stats to add:"));
-            DebugSettingsItems.Add(new DebugSettingButton("Adds random stats to the user given above.", "Do!", addRandomStats, null));
+            DebugSettingsItems.Add(new DebugSettingTextBox("username", "Username for debugging options:"));
+            DebugSettingsItems.Add(new DebugSettingButtonWithValue("randomStatsCount", "Adds random stats to the user given above.", "Do!", addRandomStats, 10.ToString(), null));
             DebugSettingsItems.Add(new DebugSettingButton("Remove all stats from the user given above.", "Do!", removeStats, null));
+            DebugSettingsItems.Add(new DebugSettingButton("Generate random time interval", "Generate!", generateRandomViewTime, null));
+            DebugSettingsItems.Add(new DebugSettingButtonWithValue("viewTime", "Add View Time to user given above.", "Do!", addViewTimeToUser, TimeSpan.Zero.ToString(), null));
             DebugSettingsItems.Add(new DebugSettingSeparator());
+            DebugSettingsItems.Add(new DebugSettingCheckBox("statsLimit", "Disable 30 second limit on stats ?", true));
             DebugSettingsItems.Add(new DebugSettingButton("Show Debug settings status.", "Show!", showStatus, null));
         }
 
         public void showStatus(params object[] parameters)
         {
-            string message = "";
-
+            string message = "".PadRight(85, '*') + "\n";
+            message += "Current status:\n\n";
+            message += "ItemType".PadRight(25) + "Description".PadRight(50) + "Value\n";
+            
             foreach (DebugSettingsItem item in MainWindow.settings.DebugSettings.Values)
             {
                 if (item is DebugSettingCheckBox)
-                    message += $"CheckBoxItem {item.Description} = {(item as DebugSettingCheckBox).CheckBoxChecked}\n";
+                    message += $"CheckBoxItem".PadRight(25) +  $"{item.Description}".PadRight(50) + $"{(item as DebugSettingCheckBox).CheckBoxChecked}\n";
 
                 else if (item is DebugSettingTextBox)
-                    message += $"TextBoxItem {item.Description} = {(item as DebugSettingTextBox).TextBoxText}\n";
+                    message += $"TextBoxItem".PadRight(25) +  $"{item.Description}".PadRight(50) +  $"{(item as DebugSettingTextBox).TextBoxText}\n";
+
+                else if (item is DebugSettingButtonWithValue)
+                    message += $"ButtonWithValueItem".PadRight(25) +  $"{item.Description}".PadRight(50) + $"{(item as DebugSettingButtonWithValue).Value}\n";
             }
 
-            MessageBox.Show("Current status:\n" + message, "Status", MessageBoxButton.OK, MessageBoxImage.Information);
+            message += "".PadRight(85, '*');
+
+            Trace.WriteLine(message);
         }
 
         public void addRandomStats(params object[] parameters)
         {
-            string username = (MainWindow.settings.DebugSettings["randomStatsUsername"] as DebugSettingTextBox).TextBoxText;
-            int count = int.Parse((MainWindow.settings.DebugSettings["randomStatsCount"] as DebugSettingTextBox).TextBoxText);
+            string username = (DebugSettings["username"] as DebugSettingTextBox).TextBoxText;
+            int count = int.Parse((DebugSettings["randomStatsCount"] as DebugSettingButtonWithValue).Value);
 
             var stats = MainWindow.settings.StreamStats.Single(item => item.Name == username);
 
@@ -63,11 +74,31 @@ namespace Twitch_Viewer
 
         public void removeStats(params object[] parameters)
         {
-            string username = (MainWindow.settings.DebugSettings["randomStatsUsername"] as DebugSettingTextBox).TextBoxText;
+            string username = (DebugSettings["username"] as DebugSettingTextBox).TextBoxText;
 
             var stats = MainWindow.settings.StreamStats.Single(item => item.Name == username);
 
             stats.ViewTimeData.Clear();
+        }
+
+        public void addViewTimeToUser(params object[] parameters)
+        {
+            string username = (DebugSettings["username"] as DebugSettingTextBox).TextBoxText;
+
+            var stats = MainWindow.settings.StreamStats.Single(item => item.Name == username);
+
+            var time = TimeSpan.Parse((DebugSettings["viewTime"] as DebugSettingButtonWithValue).Value);
+
+            stats.ViewTime = stats.ViewTime.Add(time);
+        }
+
+        public void generateRandomViewTime(params object[] parameters)
+        {
+            var r = new Random();
+
+            var time = new TimeSpan(r.Next(6), r.Next(60), r.Next(60));
+
+            (DebugSettings["viewTime"] as DebugSettingButtonWithValue).Value = time.ToString();
         }
     }
 }
