96b5e781513a46027c7a2663020a6a0789da933a
Masamas <neuanmeldungen@yahoo.de>
2016-09-01 14:23:17 +0200

    move Graph Properties to own class

---
+++ Twitch_Viewer/Types/GraphItem.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+
+namespace Twitch_Viewer.Types
+{
+    public class GraphItem : INotifyPropertyChanged
+    {
+        private StatsItem Stats { get; set; }
+
+        #region GraphProperties
+        #region GraphHeight
+        public double MondayHeight { get { return 200 * Stats.calcDayStats(DayOfWeek.Monday); } }
+
+        public double TuesdayHeight { get { return 200 * Stats.calcDayStats(DayOfWeek.Tuesday); } }
+
+        public double WednesdayHeight { get { return 200 * Stats.calcDayStats(DayOfWeek.Wednesday); } }
+
+        public double ThursdayHeight { get { return 200 * Stats.calcDayStats(DayOfWeek.Thursday); } }
+
+        public double FridayHeight { get { return 200 * Stats.calcDayStats(DayOfWeek.Friday); } }
+
+        public double SaturdayHeight { get { return 200 * Stats.calcDayStats(DayOfWeek.Saturday); } }
+
+        public double SundayHeight { get { return 200 * Stats.calcDayStats(DayOfWeek.Sunday); } }
+        #endregion // GraphHeight
+        #region GraphValues
+        public double MondayValue { get { return Stats.calcDayStats(DayOfWeek.Monday); } }
+
+        public double TuesdayValue { get { return Stats.calcDayStats(DayOfWeek.Tuesday); } }
+
+        public double WednesdayValue { get { return Stats.calcDayStats(DayOfWeek.Wednesday); } }
+
+        public double ThursdayValue { get { return Stats.calcDayStats(DayOfWeek.Thursday); } }
+
+        public double FridayValue { get { return Stats.calcDayStats(DayOfWeek.Friday); } }
+
+        public double SaturdayValue { get { return Stats.calcDayStats(DayOfWeek.Saturday); } }
+
+        public double SundayValue { get { return Stats.calcDayStats(DayOfWeek.Sunday); } }
+        #endregion // GraphValues
+        #endregion // GraphProperties
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public GraphItem(StatsItem stats)
+        {
+            Stats = stats;
+        }
+
+        protected void OnPropertyChanged(MethodBase methodBase)
+        {
+            OnPropertyChanged(methodBase.Name.Substring(4));
+        }
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public void NotifyGraphOnStatsAdded(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            if (e.Action == NotifyCollectionChangedAction.Add)
+            {
+                OnPropertyChanged("MondayHeight");
+                OnPropertyChanged("TuesdayHeight");
+                OnPropertyChanged("WednesdayHeight");
+                OnPropertyChanged("ThursdayHeight");
+                OnPropertyChanged("FridayHeight");
+                OnPropertyChanged("SaturdayHeight");
+                OnPropertyChanged("SundayHeight");
+                OnPropertyChanged("MondayValue");
+                OnPropertyChanged("TuesdayValue");
+                OnPropertyChanged("WednesdayValue");
+                OnPropertyChanged("ThursdayValue");
+                OnPropertyChanged("FridayValue");
+                OnPropertyChanged("SaturdayValue");
+                OnPropertyChanged("SundayValue");
+            }
+        }
+    }
+}
--- Twitch_Viewer/Types/StatsItem.cs
+++ Twitch_Viewer/Types/StatsItem.cs
@@ -26,39 +26,8 @@ namespace Twitch_Viewer.Types
         public string ViewTimeString { get { return XmlConvert.ToString(viewTime); } set { viewTime = XmlConvert.ToTimeSpan(value); OnPropertyChanged(MethodBase.GetCurrentMethod()); } }
         public int ViewCount { get { return viewCount; } set { viewCount = value; OnPropertyChanged(MethodBase.GetCurrentMethod()); } }
         public ObservableCollection<ViewTimeData> ViewTimeData { get { return viewTimeData; } set { viewTimeData = value; OnPropertyChanged(MethodBase.GetCurrentMethod()); BindingOperations.EnableCollectionSynchronization(ViewTimeData, lockObjectViewTimeData); } }
-
-        #region GraphProperties
-        #region GraphHeight
-        public double MondayHeight { get { return 200 * calcDayStats(DayOfWeek.Monday); } }
-
-        public double TuesdayHeight { get { return 200 * calcDayStats(DayOfWeek.Tuesday); } }
-
-        public double WednesdayHeight { get { return 200 * calcDayStats(DayOfWeek.Wednesday); } }
-
-        public double ThursdayHeight { get { return 200 * calcDayStats(DayOfWeek.Thursday); } }
-
-        public double FridayHeight { get { return 200 * calcDayStats(DayOfWeek.Friday); } }
-
-        public double SaturdayHeight { get { return 200 * calcDayStats(DayOfWeek.Saturday); } }
-
-        public double SundayHeight { get { return 200 * calcDayStats(DayOfWeek.Sunday); } }
-        #endregion
-        #region GraphValues
-        public double MondayValue { get { return calcDayStats(DayOfWeek.Monday); } }
-
-        public double TuesdayValue { get { return calcDayStats(DayOfWeek.Tuesday); } }
-
-        public double WednesdayValue { get { return calcDayStats(DayOfWeek.Wednesday); } }
-
-        public double ThursdayValue { get { return calcDayStats(DayOfWeek.Thursday); } }
-
-        public double FridayValue { get { return calcDayStats(DayOfWeek.Friday); } }
-
-        public double SaturdayValue { get { return calcDayStats(DayOfWeek.Saturday); } }
-
-        public double SundayValue { get { return calcDayStats(DayOfWeek.Sunday); } }
-        #endregion
-        #endregion
+        [XmlIgnore]
+        public GraphItem Graph { get; set; }
 
         public event PropertyChangedEventHandler PropertyChanged;
 
@@ -87,31 +56,11 @@ namespace Twitch_Viewer.Types
         public StatsItem()
         {
             ViewTimeData = new ObservableCollection<ViewTimeData>();
-            ViewTimeData.CollectionChanged += NotifyGraphOnStatsAdded;
+            Graph = new GraphItem(this);
+            ViewTimeData.CollectionChanged += Graph.NotifyGraphOnStatsAdded;
         }
 
-        private void NotifyGraphOnStatsAdded(object sender, NotifyCollectionChangedEventArgs e)
-        {
-            if (e.Action == NotifyCollectionChangedAction.Add)
-            {
-                OnPropertyChanged("MondayHeight");
-                OnPropertyChanged("TuesdayHeight");
-                OnPropertyChanged("WednesdayHeight");
-                OnPropertyChanged("ThursdayHeight");
-                OnPropertyChanged("FridayHeight");
-                OnPropertyChanged("SaturdayHeight");
-                OnPropertyChanged("SundayHeight");
-                OnPropertyChanged("MondayValue");
-                OnPropertyChanged("TuesdayValue");
-                OnPropertyChanged("WednesdayValue");
-                OnPropertyChanged("ThursdayValue");
-                OnPropertyChanged("FridayValue");
-                OnPropertyChanged("SaturdayValue");
-                OnPropertyChanged("SundayValue");
-            }
-        }
-
-        private double calcDayStats(DayOfWeek day)
+        public double calcDayStats(DayOfWeek day)
         {
             int dayStats = 0;
 
